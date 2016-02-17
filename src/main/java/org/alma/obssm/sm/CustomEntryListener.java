1928b9f1f25dfa94c9b0f8b661c85c69221264e7
Javier Fuentes <0xffaa.rm@gmail.com>
2016-02-17 15:36:14 -0300

    Added disclaimer

--- src/main/java/org/alma/obssm/sm/CustomEntryListener.java
+++ src/main/java/org/alma/obssm/sm/CustomEntryListener.java
@@ -1,5 +1,36 @@
+/*******************************************************************************
+ * ALMA - Atacama Large Millimeter Array
+ * Copyright (c) AUI - Associated Universities Inc., 2011
+ * (in the framework of the ALMA collaboration).
+ * All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * 
+ * @autor Javier Fuentes j.fuentes.m(at)icloud.com
+ * @version 0.1
+ * 
+ *******************************************************************************/
+
 package org.alma.obssm.sm;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.alma.obssm.Run;
 import org.apache.commons.scxml.SCXMLListener;
 import org.apache.commons.scxml.model.Transition;
 import org.apache.commons.scxml.model.TransitionTarget;
@@ -8,7 +39,8 @@ public class CustomEntryListener implements SCXMLListener {
 
     @Override
     public void onEntry(TransitionTarget state) {
-        System.out.println("STATE: " + state.getId());
+    	Timestamp ts = new Timestamp(System.currentTimeMillis());
+        System.out.println(ts.toString() + " STATE: " + state.getId());
     }
 
     @Override
