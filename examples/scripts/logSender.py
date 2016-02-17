5ce614193e10cc1686368f66614d6d1cb54aedfb
Javier Fuentes <0xffaa.rm@gmail.com>
2016-02-17 15:51:11 -0300

    logSender

---
+++ examples/scripts/logSender.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import socket
+import sys
+import time
+f = open(sys.argv[1],"r")
+
+lines = f.readlines()
+
+def sendLogLine(line):
+    sock = socket.socket()
+    sock.connect(("localhost", 8888))
+    sock.sendall(line)
+    sock.close()
+
+for i in lines:
+    sendLogLine(i)
+
+sendLogLine("EOF\n")
